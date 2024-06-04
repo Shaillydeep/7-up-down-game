b7fda30630aee3ca1fc662e3b801bfea08e0db72
Shaillydeep <deepshailly68@gmail.com>
2024-06-04 22:43:03 +0530

    Add endpoint for generating random numbers

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 
+const randNum = require('./controllers/randNum');
+
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 });
+
+
+app.get('/random-numbers', (req, res) => {
+    const numbers = [];
+    for (let i = 0; i < 3; i++) {
+        const randomNum = randNum.getRandomNumber();
+        numbers.push(randomNum);
+    }
+    console.log(numbers);
+    res.json(numbers);
+});
+
+
