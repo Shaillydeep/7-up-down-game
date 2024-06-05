0371ab39d66896ae0a66be9473dac31557bdb5d6
Shaillydeep <deepshailly68@gmail.com>
2024-06-05 21:25:26 +0530

    Set document title in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 
+import React, {useEffect} from 'react';
+
 import Game from './pages/Game';
 
 function App() {
+  useEffect (() => {
+    document.title = "7 Up 7 Down";
+  });
   return (
     <div className="App">
       <Game />
