11a494dbf95a6d820f4fc71cf11688b71a1849f9
Shaillydeep <deepshailly68@gmail.com>
2024-06-05 19:08:11 +0530

    Modify DiceRollResult component to use Grid and Box for mobile viewport

--- frontend/src/components/dice-roll-result.js
+++ frontend/src/components/dice-roll-result.js
@@ -1,24 +1,29 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 
 export default function DiceRollResult({ diceRollValue, displayDiceRoll }) {
     return (
         <div>
             {displayDiceRoll && (
-                <div id="dice-roll" style={ { alignContent:'center'}}>
-                    <Typography variant="h5">Dice Roll Result</Typography>
-                    <List >
-                        <ListItem alignItems="flex-start">
+                <Grid container justifyContent="center" alignItems="center">
+                    <Grid item xs={12}>
+                        <Typography variant="h5">Dice Roll Result </Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <Box sx={{ border: 1, borderRadius: 2, padding: 1 , maxWidth: 30}}>  {/* Added Box and styles */}
                             <Typography variant="h6">{diceRollValue[0]}</Typography>
-                        </ListItem>
-                        <ListItem>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <Box sx={{ border: 1, borderRadius: 2, padding: 1 , margin: 1, maxWidth: 30}}>  {/* Added Box and styles */}
                             <Typography variant="h6">{diceRollValue[1]}</Typography>
-                        </ListItem>
-                    </List>
-                </div>
+                        </Box>
+                    </Grid>
+              </Grid>
+              
             )}
         </div>
     );
