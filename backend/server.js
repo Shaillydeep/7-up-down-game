7f822f3e935e27eed50e908e9272b30c17235d51
Shaillydeep <deepshailly68@gmail.com>
2024-06-05 09:56:05 +0530

    Modify random number generation and add betting logic and endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
 const express = require('express');
 
-const randNum = require('./controllers/randNum');
+const nums = require('./controllers/nums');
+
+const numbersList = () => {
+    numbers = nums.getRandomNumberList();
+    return numbers;
+}
+
+const validBets = [100, 200, 500];
+
+let currentResult = '';
+let currentSum = 0
+
+const luckyNumber = 'luckyNumber';
+const below7 = 'below7';
+const above7 = 'above7';
+const win = 'win';
+const loss = 'loss';
+const luckyWin = 'luckyWin';
 
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
@@ -11,13 +30,95 @@ app.listen(3000, () => {
 
 
 app.get('/random-numbers', (req, res) => {
-    const numbers = [];
-    for (let i = 0; i < 3; i++) {
-        const randomNum = randNum.getRandomNumber();
-        numbers.push(randomNum);
-    }
+    numbers = numbersList();
+    /* numbers = [3,4] */
     console.log(numbers);
+
     res.json(numbers);
+
+    currentSum = nums.sumList(numbers);
+    console.log(currentSum);
+});
+
+app.get('/check-choice/:playerChoice', (req, res) => {
+    const playerChoice = req.params.playerChoice;
+    let result;
+
+    switch (playerChoice) {
+        case luckyNumber:
+            if (currentSum === 7) {
+                result = luckyWin;
+            } else {
+                result = loss;
+            }
+            break;
+
+        case below7:
+            if (currentSum < 7) {
+                result = win;
+            } else {
+                result = loss;
+            }
+            break;
+
+        case above7:
+            if (currentSum > 7) {
+                result = win;
+            } else {
+                result = loss;
+            }
+            break;
+        default:
+            /* return error */
+            result = 'invalid';
+    }
+
+    currentResult = result;
+
+    console.log(result);
+    res.send(result);
+});
+
+app.post('/update-amount', (req, res) => {
+    if (!req.body || !req.body.amount || !req.body.bet) {
+        res.status(400).send('Missing amount or bet in request body');
+        return;
+    }    
+    
+    const { amount, bet } = req.body;
+
+    if (!validBets.includes(bet)) {
+        res.status(400).send('Invalid bet');
+        return;
+    }
+
+    let updatedAmount;
+
+    console.log(amount, bet);
+    console.log(currentResult);
+
+    switch (currentResult) {
+        case win:
+            updatedAmount = amount + bet * 2;
+            break;
+
+        case loss:
+            updatedAmount = amount;
+            break;
+
+        case luckyWin:
+            updatedAmount = amount + bet * 5;
+            break;
+
+        default:
+            /* return error */
+            res.status(400).send('Invalid current result');
+            return;
+    }
+
+    console.log(updatedAmount);
+
+    res.send({ updatedAmount });
 });
 
 
